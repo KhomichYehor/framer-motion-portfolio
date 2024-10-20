f438d100dbe8e1ac47042d70caec94b4ce5f5922
Yehor Khomich <eee.houmich@gmail.com>
2024-10-20 17:26:55 +0300

    Complete NavBar component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Image from 'next/image';
+import cursor from '../assets/icon1.png';
+import message from '../assets/icon2.png';
+import { motion } from 'framer-motion';
+import profilepic from '../assets/profilepic.png';
+
+const Hero = () => {
+    return (
+        <div className='py-24 relative overflow-clip bg-[linear-gradient(to_bottom,#000,#2B1942_35%,#8F5C55_60%,#DBAF6E_80%)]'>Hero</div>
+    )
+}
+
+export default Hero;
